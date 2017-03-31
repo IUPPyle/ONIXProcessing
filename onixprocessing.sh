b48c37d2b4448d130d4833762c3098d7604f1b2f
IUPPyle <dapyle@indiana.edu>
2017-03-31 11:00:23 -0400

    Add files via upload

--- onixprocessing.sh
+++ onixprocessing.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-# This script was written by Dan Pyle, and requires two arguments: 
-# the starting file name, and the ending file name. You must run from the directory the files are in, or specify the path.
+# Script written by Dan Pyle, 3-31-2017
+# This script was written by Dan Pyle, and requires one argument, the starting file name, and will be overwritten. 
+# You must run from the directory the file is in, or specify the path.
 
 # transform the file, returning only characters we want and ignoring the rest, and outputs to temp 'foo.xml'
 tr -cd '\11\12\15\40-\176' < "$1" > foo.xml
+
+# format the ONIX file to be human readable
 xmllint --format foo.xml > "$1"
-# rm "$1"
+
+# remove temp file
 rm foo.xml
